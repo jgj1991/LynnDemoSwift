a0fbbcb05bfd2e82494d2e4b6754ea4f486d4bfc
姬冠杰 <15121139605@163.com>
2018-11-27 09:57:00 +0800

    添加网络请求类

--- LynnSwiftDemo/LynnSwiftDemo/ViewController.swift
+++ LynnSwiftDemo/LynnSwiftDemo/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import HandyJSON
+let url = "/v22/index.php/Home/misc/banners"
+
+struct FLBannerModel: HandyJSON {
+    let banner_name: String = ""
+    let banner_image_id: String = ""
+    let banner_type: String = ""
+    let banner_post_id: String = ""
+    let banner_tag_id: String = ""
+    let banner_h5_link: String = ""
+    let banner_image_url: String = ""
+}
+
 
 class ViewController: UIViewController {
-
+    
+    var datas: [FLBannerModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Network.default.request(CommonTargetTypeApi.getRequest(url, [: ]), successClosure: { (json) in
+            guard let parameter = json.dictionaryObject,
+                let data = parameter["data"] as? Array<Any>
+                else { return }
+            if let items = [FLBannerModel].deserialize(from: data) as? [FLBannerModel] {
+                self.datas = items
+            }
+        }) { (error) in
+            
+        }
     }
-
-
 }
 
